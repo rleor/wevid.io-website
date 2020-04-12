80c79fc85d846339002a5f83e3830e17eed5d5ad
leoaion <leo.ren@theoan.com>
2020-04-12 16:51:59 +0800

    fix warning

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 import HeroFull from '../components/sections/HeroFull';
 import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Pricing from '../components/sections/Pricing';
-import Testimonial from '../components/sections/TestimonialCarousel';
+// import Pricing from '../components/sections/Pricing';
+// import Testimonial from '../components/sections/TestimonialCarousel';
 
 class Home extends React.Component {
   render() {
