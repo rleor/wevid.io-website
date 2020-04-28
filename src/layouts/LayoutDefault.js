df455be9ca6f60d319581bbb80e41c9055147a1b
leoaion <leo.ren@theoan.com>
2020-04-28 11:59:31 +0800

    remove footer

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -8,7 +8,7 @@ const LayoutDefault = ({ children }) => (
     <main className="site-content">
       {children}
     </main>
-    <Footer topDivider className="illustration-section-09" />
+    {/* <Footer topDivider className="illustration-section-09" /> */}
   </React.Fragment>
 );
 
